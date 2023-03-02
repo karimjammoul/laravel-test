a7e9990a52c47cabc8abb0f5a515e19ad6f20edc
Karim Jammoul <karimjammoul9@gmail.com>
2023-03-02 23:30:48 +0200

    redirect to posts index

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('posts.index');
 });
 
 Route::resource('posts', \App\Http\Controllers\PostController::class);
